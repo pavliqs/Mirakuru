a2245def5c943d63f2f76582fb5b76c564bc2246
Swordf1sh <uripatton@gmail.com>
2015-11-15 13:47:24 +0400

    printf changed to data

--- libs/syntax.py
+++ libs/syntax.py
@@ -49,7 +49,7 @@ class MyHighlighter(QSyntaxHighlighter):
         # output
         brush = QBrush(Qt.magenta, Qt.SolidPattern)
         specialConstant.setForeground(brush)
-        keywords = QStringList(["printf", ])
+        keywords = QStringList(["data", ])
         for word in keywords:
             pattern = QRegExp("\\b" + word + "\\b")
             rule = HighlightingRule(pattern, specialConstant)
